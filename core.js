8d03526d9f36deadb27adb155fd7cc96842ecdfd
mandy1339 <mandy1339@gmail.com>
2017-07-31 00:52:40 -0400

    commit 3

--- core.js
+++ core.js
@@ -1,154 +1,155 @@
 
-
-    var $ = require("jquery");
-
-
-
 var config = require('./config');
 var Twit = require('twit');
 twitter = new Twit(config);
+var request = require('request');
+
+//DECLARE SOME VARIABLES
+var areTheyOn = undefined;  //boolean
+
+
+//lightsOff
+//lightsOn
+//lightsRed
+//lightsGreen
+//lightsCustomHue
+//areLightsOn (TAKES TIME SETTING THE VARIABLE)
+//flashLights
+//snorlaxLightAlert()
+//getCurrentHue
+
+//alertIfSnorlax();
+//flashLights();
+//areLightsOn();
 
 
 
-twitter.get('statuses/user_timeline', {screen_name: "kraken1339", count: 1}, function(err, data, response){
-  console.log(data[0].text);
-})
 
+
+
+
+
+
+
+//CALLS TO BRIDGE
+//--------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------
+
+function areLightsOn(callback) {
+  request.get(
+    'http://192.168.1.5/api/3MWz5lzDLxmE3b-CEBB93hBgyiP2DuERszBLWjDf/groups/1',
+    {json:true},
+    function (error, response, body) {
+      console.log('body:', body);
+      areTheyOn = body.state.all_on; if(callback){callback()};}
+  );
+}
+
+function lightsOff() {
+  request.put('http://192.168.1.5/api/3MWz5lzDLxmE3b-CEBB93hBgyiP2DuERszBLWjDf/groups/1/action', {form:'{"on":false}'}, function (error, response, body){
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+}
+
+function lightsOn() {
+  request.put('http://192.168.1.5/api/3MWz5lzDLxmE3b-CEBB93hBgyiP2DuERszBLWjDf/groups/1/action', {form:'{"on":true}'}, function (error, response, body){
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+}
+
+function lightsBlue() {
+  request.put('http://192.168.1.5/api/3MWz5lzDLxmE3b-CEBB93hBgyiP2DuERszBLWjDf/groups/1/action', {form:'{"hue":46920}'}, function (error, response, body){
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+}
+
+function lightsRed() {
+  request.put('http://192.168.1.5/api/3MWz5lzDLxmE3b-CEBB93hBgyiP2DuERszBLWjDf/groups/1/action', {form:'{"hue":0}'}, function (error, response, body){
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+}
+
+function lightsGreen() {
+  request.put('http://192.168.1.5/api/3MWz5lzDLxmE3b-CEBB93hBgyiP2DuERszBLWjDf/groups/1/action', {form:'{"hue":25500}'}, function (error, response, body){
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+}
+
+function lightsWhite() {
+  request.put('http://192.168.1.5/api/3MWz5lzDLxmE3b-CEBB93hBgyiP2DuERszBLWjDf/groups/1/action', {form:'{"hue":32000}'}, function (error, response, body){
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+}
+
+function lightsCustomHue(hue) {
+  var bodyData = '{"hue":' + hue + '}';
+  request.put('http://192.168.1.5/api/3MWz5lzDLxmE3b-CEBB93hBgyiP2DuERszBLWjDf/groups/1/action', {form:bodyData}, function (error, response, body){
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+}
+
+function getCurrentHue() {
+  var hueNow = 0;
+  request.get('http://192.168.1.5/api/3MWz5lzDLxmE3b-CEBB93hBgyiP2DuERszBLWjDf/groups/1', function (error, response, body){
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    hueNow = parseFloat(JSON.stringify(response.action.hue))
+  });
+}
+
+function flashLights() {
+  var hueNow = 0;
+  request.put('http://192.168.1.5/api/3MWz5lzDLxmE3b-CEBB93hBgyiP2DuERszBLWjDf/groups/1/action', {form:'{"alert":"select"}'}, function (error, response, body){
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+
+  });
+}
+
+
+
+
+//OTHER FUNCTIONS
+
+function snorlaxLightAlert() {
+  lightsBlue();
+  setTimeout(flashLights, 100);
+  setTimeout(lightsWhite, 1200);
+  setTimeout(flashLights, 1400);
+  setTimeout(lightsBlue, 2600);
+  setTimeout(flashLights,2800);
+}
 
 function alertIfSnorlax() {
-  twitter.get('statuses/user_timeline', {screen_name: "kraken1339", count: 1}, function(err, data, response){
-    if((data[0].text) == "location 2 hunna") {
-      console.log('FLASH HERE FLASH HERE');
-      flashLights();
-    }
-    else {
-
-      console.log('NOT FLASHING :()');
-      console.log(data[0].text);
+  twitter.get('statuses/user_timeline', {screen_name: "PGANVACentralCh", count: 1}, function(err, data, response){
+    for(i = 0; i < data.length; i++) {
+      console.log(data[i].text, '\n\n');
+      if(data[i].text.toLowerCase().includes('snorlax')) {
+        console.log('SNORLAX IS OUT THERE');
+        snorlaxLightAlert();
+      }
     }
   })
 }
 
-alertIfSnorlax();
 
 
 
-
-
-
-//AJAX CALLS TO BRIDGE
-//--------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------
-function lightsOff() {
-    $.ajax({
-        async: false,
-        type: 'PUT',
-        url: 'http://192.168.1.5/api/hnpWKAUC0llMVOzIwfVnnjOfuGQ1aLF0Q6uNaGDn/groups/1/action',
-        data: '{"on":false}',
-        async: false
-    })
-    .done(function(response) {
-        console.log(JSON.stringify(response));
-    });
-}
-
-function lightsOn() {
-    $.ajax({
-        type: 'PUT',
-        url: 'http://192.168.1.5/api/hnpWKAUC0llMVOzIwfVnnjOfuGQ1aLF0Q6uNaGDn/groups/1/action',
-        data: '{"on":true}',
-        async: false
-    })
-    .done(function(response) {
-        console.log(JSON.stringify(response));
-    });
-}
-
-function lightsRed() {
-    $.ajax({
-        async: false,
-        type: 'PUT',
-        url: 'http://192.168.1.5/api/hnpWKAUC0llMVOzIwfVnnjOfuGQ1aLF0Q6uNaGDn/groups/1/action',
-        data: '{"hue":0}',
-        async: false
-    })
-    .done(function(response) {
-        console.log(JSON.stringify(response));
-    });
-}
-
-function lightsGreen() {
-    $.ajax({
-        type: 'PUT',
-        url: 'http://192.168.1.5/api/hnpWKAUC0llMVOzIwfVnnjOfuGQ1aLF0Q6uNaGDn/groups/1/action',
-        data: '{"hue":25500}',
-        async: false
-    })
-    .done(function(response) {
-        console.log(JSON.stringify(response));
-    });
-}
-
-function lightsCustomHue(hue) {
-    var bodyData = '{"hue":' + hue + '}';
-    $.ajax({
-        async: false,
-        type: 'PUT',
-        url: 'http://192.168.1.5/api/hnpWKAUC0llMVOzIwfVnnjOfuGQ1aLF0Q6uNaGDn/groups/1/action',
-        data: bodyData,
-        async: false
-    })
-    .done(function(response) {
-        console.log(JSON.stringify(response));
-    });
-}
-
-function getCurrentHue() {
-    var hueNow = 0;
-    $.ajax({
-        async: false,
-        type: 'GET',
-        url: 'http://192.168.1.5/api/hnpWKAUC0llMVOzIwfVnnjOfuGQ1aLF0Q6uNaGDn/groups/1',
-        async: false
-    })
-    .done(function(response) {
-        console.log(JSON.stringify(response.action.hue));
-        hueNow = parseFloat(JSON.stringify(response.action.hue));
-    });
-    return hueNow;
-}
-
-function flashLights() {
-    $.ajax({
-        type: 'PUT',
-        url: 'http://192.168.1.5/api/hnpWKAUC0llMVOzIwfVnnjOfuGQ1aLF0Q6uNaGDn/groups/1/action',
-        data: '{"alert":"select"}',
-        async: false
-    })
-    .done(function(response) {
-        console.log(JSON.stringify(response));
-    });
-}
-
-function areLightsOn() {
-    $.ajax({
-        type: 'GET',
-        url: 'http://192.168.1.5/api/hnpWKAUC0llMVOzIwfVnnjOfuGQ1aLF0Q6uNaGDn/groups/1',
-        async: false
-    })
-    .done(function(response) {
-        console.log(JSON.stringify(response.state.all_on));
-    });
-}
-
-
-
-
-
-
-
-
-
-
-
-//PGANVACentralCh
+//MAIN
+setInterval(alertIfSnorlax, 5000);
